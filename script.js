e4d57bb203758c57ee8ae57a7377218540ee011d
samuel <smfikiru@gmail.com>
2026-07-09 03:34:31 +0300

    added renderNOItem function

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-const shoppingList = [
-  {
-    itemName: "Shopping List",
-  },
-  {
-    itemName: "Jordan Shoes",
-  },
-  {
-    itemName: "Throusar",
-  },
+const shoppingListArray = [
+
 ];
 
 const shoppingListContainer = document.querySelector(
   ".js-shopping-list-container",
 );
+const noItemContainer = document.querySelector('.no-item-container');
+const bodyContainer = document.querySelector('.js-body-container');
 
 renderShoppingList();
 function renderShoppingList(){
 let code = ``;
-shoppingList.forEach((item) => {
+shoppingListArray.forEach((item) => {
   const itemName = item.itemName;
   code += `
     <div class="shopping-row-container">
@@ -34,3 +28,26 @@ shoppingList.forEach((item) => {
 shoppingListContainer.innerHTML = code;    
 }
 
+renderNoItem();
+function renderNoItem(){
+let ifExists = `
+        <div class="filtering-container">
+          <button class="filter">All</button>
+          <button class="filter">Bought</button>
+          <button class="filter">Not Bought</button>
+        </div>
+        <div class="shopping-list-container js-shopping-list-container"></div>
+
+        <div class="clear-container">
+          <button class="clear-all-btn">Clear All</button>
+          <button class="clear-completed-btn">Clear Completed</button>
+        </div>
+ ` ;
+const ifEmpty = `
+ <div class="no-item-container js-no-item-container">
+    <p class="no-item-text">There are no items, please add an item</p>
+ </div>
+ `
+const code = (shoppingListArray.length === 0)? ifEmpty: ifExists;
+bodyContainer.innerHTML = code;    
+}
