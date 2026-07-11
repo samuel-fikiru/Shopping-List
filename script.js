369178b23d507ece93e98e92c1feea1154ec6eaf
samuel <smfikiru@gmail.com>
2026-07-11 18:46:33 +0300

    converted htm codes into customs js element creation for shopping list

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const shoppingListArray = [
   {
     itemName: "Shoes",
+    id: 0,
   },
   {
     itemName: "Shoes",
+    id: 1,
   },
   {
     itemName: "Shoes",
+    id: 2,
   },
 ];
 
@@ -21,28 +24,31 @@ const inputBar = document.querySelector(".list-input");
 renderNoItem();
 renderShoppingList();
 
-inputBar.addEventListener('keydown', (event)=>{
-  if (event.key === 'Enter'){
+inputBar.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
     addItem();
     renderShoppingList();
-    inputBar.value = '';  
+    inputBar.value = "";
   }
-})
+});
 addButton.addEventListener("click", () => {
   addItem();
   renderShoppingList();
-  inputBar.value = '';
+  inputBar.value = "";
 });
 
 function addItem() {
   const newObj = {
     itemName: `${inputBar.value}`,
+    id: Date.now(),
   };
   shoppingListArray.push(newObj);
 }
 
 function renderShoppingList() {
+  /*
   let code = ``;
+  
   shoppingListArray.forEach((item) => {
     const itemName = item.itemName;
     code += `
@@ -56,8 +62,48 @@ function renderShoppingList() {
         </div>
     </div>
     `;
+    */
+  shoppingListArray.forEach((item) => {
+    const itemName = item.itemName;
+    const container = document.createElement("div");
+    container.className = "shopping-row-container";
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.className = "checkbox";
+    container.appendChild(input);
+    const p = document.createElement("p");
+    p.className = "things-to-buy";
+    p.textContent = itemName;
+    container.appendChild(p);
+    const subContainer = document.createElement("div");
+    subContainer.className = "icons-container";
+    imgArray = [
+      {
+        class: "drag-icon",
+        src: "icons/drag.png",
+        alt: "Drag",
+      },
+      {
+        class: "edit-icon",
+        src: "icons/edit.png",
+        alt: "EDIT",
+      },
+      {
+        class: "delete-icon",
+        src: "icons/trash.png",
+        alt: "TRASH",
+      },
+    ];
+    imgArray.forEach((data) => {
+      const img = document.createElement("img");
+      img.className = data.class;
+      img.src = data.src;
+      img.alt = data.alt;
+      subContainer.appendChild(img);
+    });
+    container.appendChild(subContainer);
+    shoppingListContainer.appendChild(container);
   });
-  shoppingListContainer.innerHTML = code;
 }
 
 function renderNoItem() {
